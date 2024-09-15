df3bee80042d456057c144520725ce14646c7201
Mustafa1046 <mustafa104637@gmail.com>
2024-09-15 18:09:50 +0500

    JavaScript Add

---
+++ js/main.js
@@ -0,0 +1,9 @@
+        //   typed of
+
+const typed=new Typed(".multiple-text",{
+    strings: ['Full Stack Web Developer','Frontend Deveolper', 'Web Designing'],
+    typeSpeed: 70,
+    backSpeed:70,
+    backDelay: 1000,
+    loop:true,
+    });
